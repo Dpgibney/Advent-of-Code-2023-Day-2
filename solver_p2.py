3370578c5fe959879a419c2a0ccba3c309e8f13a
Daniel Gibney <dpgibney@gmail.com>
2023-12-26 13:30:21 -0600

    Cleaned up part 2 a bit

--- solver_p2.py
+++ solver_p2.py
@@ -1,11 +1,10 @@
-values = 0
+value = 0
 
 file1 = open('p1_input.txt','r')
 #file1 = open('p1_input.example','r')
 Lines = file1.readlines()
 
 #red, green, blue
-num_marbles = [12,13,14]
 
 for index, games in enumerate(Lines):
     max_marbles = {"green":0,"blue":0,"red":0}
@@ -14,13 +13,11 @@ for index, games in enumerate(Lines):
     games = games.split(";")
     for game in games:
         tmp = game.split()
-        print("game:",index+1,tmp)
         for i in range(len(tmp)-1):
             if i % 2 == 0:
                 if max_marbles[tmp[i+1]] < int(tmp[i]):
                     max_marbles[tmp[i+1]] = int(tmp[i])
-    print(max_marbles)
-    values += (max_marbles["green"] * max_marbles["red"] * max_marbles["blue"])
+    value += (max_marbles["green"] * max_marbles["red"] * max_marbles["blue"])
 
-print(values)
+print(value)
 
